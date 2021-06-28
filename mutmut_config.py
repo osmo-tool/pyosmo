ed334ce9b1059e26de9c46ef0e1dba417ddf6762
Olli-Pekka Puolitaival <olli-pekka@puolitaival.fi>
2021-06-28 23:33:33 +0300

    Skip logging

--- mutmut_config.py
+++ mutmut_config.py
@@ -1,3 +1,6 @@
 def pre_mutation(context):
     if context.filename == 'setup.py':
         context.skip = True
+    line = context.current_source_line.strip()
+    if line.startswith('log'):
+        context.skip = True
