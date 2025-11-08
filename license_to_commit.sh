aa5b4639817f4b0027bbca223bf7756902c2e2cb
Olli-Pekka Puolitaival <olli-pekka@puolitaival.fi>
2025-11-08 22:56:38 +0200

    Add `mypy` as a dependency and include it in development and CI workflows

--- license_to_commit.sh
+++ license_to_commit.sh
@@ -1,4 +1,6 @@
 #/bin/bash
-pytest pyosmo/tests/
-pylint *
-flake8 --max-line-length 120 --ignore=E722,F401,E402
+uv sync --all-extras
+uv run pytest pyosmo/tests/
+uv run pylint *
+uv run flake8 --max-line-length 120 --ignore=E722,F401,E402
+uv run mypy pyosmo/
